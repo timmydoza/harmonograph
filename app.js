b3a52104448cc479ffbc577b157c65f1806af6ca
Tim Mendoza <timmydoza@gmail.com>
2019-03-13 11:23:45 -0600

    Get controls working

--- app.js
+++ app.js
@@ -1,55 +1,26 @@
-const context = document.querySelector('canvas').getContext('2d');
-
-const size = window.innerHeight < window.innerWidth ? window.innerHeight : window.innerWidth;
-
-const canvas = d3
-  .select('canvas')
-  .attr('width', size)
-  .attr('height', size);
-
-const scale = d3
-  .scaleLinear()
-  .domain([-2, 2])
-  .range([0, size]);
-
-const xFreq = 3;
-const yFreq = 2;
-const zFreq = 3.01;
-const invert = false;
-const radial = true;
-const speed = 2;
-
-const line = d3
-  .line()
-  .x(d => scale(d[0]))
-  .y(d => scale(d[1]));
-
-function update(min, max) {
-  const data = d3.range(min, max).map(d => {
-    const theta = (d / 180) * Math.PI;
-    let x = Math.cos(theta * xFreq);
-    let y = Math.sin(theta * yFreq);
-
-    if (radial) {
-      x += Math.cos(theta * zFreq);
-      y += Math.sin(theta * zFreq);
-
-      if (invert) {
-        x /= zFreq;
-        y /= zFreq;
-      }
-    }
-
-    return [x, y];
-  });
-
-  context.beginPath();
-  line.context(context)(data);
-  context.stroke();
-}
-
-let last = 0;
-const timer = d3.timer(elapsed => {
-  update(last * speed, elapsed * speed + 1);
-  last = elapsed;
+const app = new Vue({
+  el: '.container',
+  data: {
+    settings: {
+      xFreq: 3,
+      yFreq: 2,
+      zFreq: 3.01,
+      invert: false,
+      type: 'rotary',
+      speed: 0.2,
+      decay: 0
+    },
+    harmonograph: Harmonograph()
+  },
+  mounted: function() {
+    this.harmonograph.init();
+    this.harmonograph.updateSettings(this.settings);
+    this.harmonograph.startAnimation();
+  },
+  updated: function() {
+    console.log(this.settings);
+    this.harmonograph.updateSettings(this.settings);
+    this.harmonograph.stopAnimation();
+    this.harmonograph.startAnimation();
+  }
 });
---
+++ harmonograph.js
@@ -0,0 +1,70 @@
+function Harmonograph() {
+  let context, size, scale, timer, settings, line;
+
+  function update(min, max) {
+    const { xFreq, yFreq, zFreq, type, invert, decay } = settings;
+    const data = d3.range(min, max, 0.1).map(d => {
+      const theta = (d / 180) * Math.PI;
+      let x = Math.cos(theta * xFreq);
+      let y = Math.sin(theta * yFreq);
+
+      if (type === 'rotary') {
+        x += Math.cos(theta * zFreq);
+        y += Math.sin(theta * zFreq);
+
+        if (invert) {
+          x /= zFreq;
+          y /= zFreq;
+        }
+      }
+
+      return [x, y];
+    });
+
+    context.beginPath();
+    line.context(context)(data);
+    context.stroke();
+  }
+
+  function init() {
+    context = document.querySelector('canvas').getContext('2d');
+
+    canvasContainer = document.querySelector('.canvasContainer');
+    size = window.innerHeight < canvasContainer.offsetWidth ? window.innerHeight : canvasContainer.offsetWidth;
+    size -= 40; // Account for padding
+
+    canvas = d3
+      .select('canvas')
+      .attr('width', size)
+      .attr('height', size);
+
+    scale = d3
+      .scaleLinear()
+      .domain([-2, 2])
+      .range([0, size]);
+
+    line = d3
+      .line()
+      .x(d => scale(d[0]))
+      .y(d => scale(d[1]));
+  }
+
+  function startAnimation() {
+    context.clearRect(0, 0, size, size);
+    let last = 0;
+    timer = d3.timer(elapsed => {
+      update(last * settings.speed, elapsed * settings.speed + 1);
+      last = elapsed;
+    });
+  }
+
+  function stopAnimation() {
+    timer && timer.stop();
+  }
+
+  function updateSettings(_) {
+    settings = _;
+  }
+
+  return { init, startAnimation, stopAnimation, updateSettings };
+}
